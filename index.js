12be6587358d1e8cc4004ac774bbdb1eb9981b6a
ayshi roy <ayshiroy22@gmail.com>
2020-09-27 12:07:09 -0700

    my first node  commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+
+const app = express();
+
+//const rootCall = (req, res) => res.send('thank you very much');
+
+app.get('/',(req, res) => {
+    res.send('thank you for calling me');
+})
+
+app.listen(3000, () =>console.log('Listenting to port 3000'));
